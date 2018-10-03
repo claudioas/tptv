dfa623e0f3476b2cb49e5c8a118ab168cc1239fc
Claudio Sepúlveda <claudiosepulveda.ac@gmail.com>
2018-10-02 22:10:55 -0300

    bodega y login

---
+++ application/views/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<script>
+		let base_url = '<?php echo base_url() ?>';
+	</script>
+	<title>Home</title>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+</head>
+
+<body>
+	<div id="app">
+		<template>
+  <v-app id="inspire">
+		<v-toolbar color="blue darken-3" dark app fixed>
+			<v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+				<span class="hidden-sm-and-down">TPT</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+		</v-toolbar>
+		<v-content>
+			<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Acceso</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+		</v-content>
+		</v-app>
+		</template>
+	</div>
+
+	<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
+	<script src="<?php echo base_url() ?>js/index.js"></script>
+</body>
+
+</html>
