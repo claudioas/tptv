ab7b4f8dc7738a18c23a8abcb72808c313963e1b
Claudio Sepúlveda <claudiosepulveda.ac@gmail.com>
2018-11-01 16:23:34 -0300

    [LOGIN] Acceso Planificación y Operaciones
    vista para cada uno

--- application/views/index.php
+++ application/views/index.php
@@ -22,29 +22,63 @@
 					<v-spacer></v-spacer>
 				</v-toolbar>
 				<v-content>
+
 					<template>
 						  <v-app id="inspire">
 						    <v-content>
 						      <v-container fluid fill-height>
 						        <v-layout align-top justify-center>
-						          <v-flex xs12 sm8 md4>
-						            <v-card class="elevation-12">
-						              <v-toolbar dark color="primary">
-						                <v-toolbar-title>Indentifícate</v-toolbar-title>
-						                <v-spacer></v-spacer>
-						              </v-toolbar>
-						              <v-card-text>
-						                <v-form>
-						                  <v-text-field prepend-icon="person" name="login" label="Usuario" type="text" v-model="u"></v-text-field>
-						                  <v-text-field id="password" prepend-icon="lock" name="password" label="Clave" type="password" v-model="p"></v-text-field>
-						                </v-form>
-						              </v-card-text>
-						              <v-card-actions>
-						                <v-spacer></v-spacer>
-						                <v-btn color="primary" v-on:click="login()">Ingresar</v-btn>
-						              </v-card-actions>
-						            </v-card>
-						          </v-flex>
+											<v-tabs
+											  color="cyan"
+											  dark
+											  slider-color="yellow"
+												width="500px"
+											>
+											  <v-tab ripple>
+											    Operaciones
+											  </v-tab>
+											  <v-tab ripple>
+											    Planificación
+											  </v-tab>
+											  <v-tab-item>
+											    <v-card flat>
+														<v-flex xs12 sm12 md12>
+									            <v-card class="elevation-12">
+									              <v-toolbar dark color="primary">
+									                <v-toolbar-title>Indentifícate</v-toolbar-title>
+									                <v-spacer></v-spacer>
+									              </v-toolbar>
+									              <v-card-text>
+									                <v-form>
+									                  <v-text-field prepend-icon="person" name="login" label="Usuario" type="text" v-model="u"></v-text-field>
+									                  <v-text-field id="password" prepend-icon="lock" name="password" label="Clave" type="password" v-model="p"></v-text-field>
+									                </v-form>
+									              </v-card-text>
+									              <v-card-actions>
+									                <v-spacer></v-spacer>
+									                <v-btn color="primary" v-on:click="login()">Ingresar</v-btn>
+									              </v-card-actions>
+									            </v-card>
+									          </v-flex>
+											    </v-card>
+											  </v-tab-item>
+											  <v-tab-item>
+											    <v-card flat>
+														<v-flex xs12 sm12 md12>
+									            <v-card class="elevation-12">
+									              <v-toolbar dark color="primary">
+									                <v-toolbar-title>Google</v-toolbar-title>
+									                <v-spacer></v-spacer>
+									              </v-toolbar>
+									              <v-card-text center>
+																	<v-btn color="primary" v-on:click="loginGoogle()">Ingresar</v-btn>
+									              </v-card-text>
+									            </v-card>
+									          </v-flex>
+											    </v-card>
+											  </v-tab-item>
+											</v-tabs>
+
 						        </v-layout>
 						      </v-container>
 						    </v-content>
