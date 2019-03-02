83eb73a1da1a612690d3b2bacd8342c57bf06315
Claudio Sepúlveda <claudiosepulveda.ac@gmail.com>
2019-03-02 02:35:11 -0300

    [BODEGA] validaciones

--- application/models/bodega/bodega_m.php
+++ application/models/bodega/bodega_m.php
@@ -8,13 +8,13 @@ class bodega_m extends CI_Model{
   }
 
   function recibirReferencia($referencia){
-    return $this->db->query("select * from referencias where ref_referencia = '".$referencia."' ")->result_array();
+    return $this->db->query("select * from referencias where ref_referencia = '".$referencia."' and ref_estado = 'ENVIADA' and ref_estado != '' ")->result_array();
   }
 
   function listarReferencias(){
     return $this->db->query("select ref_id,ref_referencia,ref_ot,ref_articulo,ref_um,ref_lote,ref_cantidadEnvase,ref_cantidadxEnvase,ref_umcantidadxEnvase,ref_total,ref_ubicacion,ref_estado,ref_dominio,ref_tra,ref_tipo
                              from referencias
-                             where ref_estado = 'ENVIADA'")->result_array();
+                             where ref_estado = 'ENVIADA' and ref_tra != '' ")->result_array();
   }
 
 
